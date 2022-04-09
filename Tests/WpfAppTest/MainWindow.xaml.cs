70e5a180b5238a69c876c3af11b312608902f996
kanadeiar <kanadeiar@gmail.com>
2022-04-09 21:07:37 +0300

    Шифрование методом Цезаря

--- Tests/WpfAppTest/MainWindow.xaml.cs
+++ Tests/WpfAppTest/MainWindow.xaml.cs
@@ -1,7 +1,23 @@
-﻿namespace WpfAppTest
+﻿using Kanadeiar.Core.Encoders;
+
+namespace WpfAppTest
 {
     public partial class MainWindow
     {
         public MainWindow() => InitializeComponent();
+
+        private void ButtonTestEncrypt_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var text = TextBoxTo.Text;
+            var result = CaesarEncoder.Encode(text);
+            TextBoxFrom.Text = result;
+        }
+
+        private void ButtonTextDecrypt_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var text = TextBoxFrom.Text;
+            var result = CaesarEncoder.Decode(text);
+            TextBoxReturn.Text = result;
+        }
     }
 }
