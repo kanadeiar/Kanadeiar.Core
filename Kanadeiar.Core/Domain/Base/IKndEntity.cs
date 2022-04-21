433e22a73aebb8a67dd4452d92cb95151c02681f
kanadeiar <kanadeiar@gmail.com>
2022-04-22 00:57:52 +0300

    Добавлены базовые сущности

---
+++ Kanadeiar.Core/Domain/Base/IKndEntity.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kanadeiar.Core.Domain.Base;
+
+/// <summary>
+/// Базовая сущность библиотек
+/// </summary>
+public interface IKndEntity<TId> : IKndEntity
+{
+    /// <summary>
+    /// Идентификатор
+    /// </summary>
+    [Key]
+    public TId Id { get; set; }
+}
+
+/// <summary>
+/// Базовая сущность библиотек
+/// </summary>
+public interface IKndEntity
+{
+}
